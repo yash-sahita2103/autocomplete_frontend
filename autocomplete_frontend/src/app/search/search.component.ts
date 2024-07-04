99da657e84b6b347ee53959d22af47d95999048d
yash.sahita2103 <yash.sahita2103@gmail.com>
2024-07-05 03:22:04 +0530

    fixed no suggestion found bug and added functionality

--- autocomplete_frontend/src/app/search/search.component.ts
+++ autocomplete_frontend/src/app/search/search.component.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
-import { map, startWith, switchMap  } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { catchError, map, startWith, switchMap, filter } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
-
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
   myControl = new FormControl();
   filteredOptions: Observable<string[]> | undefined;
 
@@ -19,16 +18,28 @@ export class SearchComponent {
   ngOnInit() {
     this.filteredOptions = this.myControl.valueChanges.pipe(
       startWith(''),
+      filter(value => value.trim().length > 0), // Filter out empty queries
       switchMap(value => this._filter(value))
     );
   }
 
   private _filter(value: string): Observable<string[]> {
-    const filterValue = value.toLowerCase();
-    return this.http.get<{ suggestions: string[] }>(`http://localhost:5000/autocomplete?query=${filterValue}`)
+    const filterValue = value.toLowerCase().trim();
+    if (filterValue.length === 0) {
+      return of([]); // Return an empty observable if query is empty
+    }
+
+    return this.http.get<string[]>(`http://localhost:5000/autocomplete?query=${filterValue}`)
       .pipe(
-        map(response => response.suggestions)
+        map(response => {
+          if (response.length === 0) {
+            return ['No suggestions found'];
+          }
+          return response;
+        }),
+        catchError(() => {
+          return of(['No suggestions found']);
+        })
       );
   }
 }
-
